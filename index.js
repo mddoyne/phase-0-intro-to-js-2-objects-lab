d9a930dd6e46bf259386ea2c235eab841ae7ac2f
mark <mddoyne@asu.edu>
2022-04-13 22:31:02 -0600

    save

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {};
+
+employee.name = "Mark";
+employee.address = "Colorado";
+
+function updateEmployeeWithKeyAndValue(employee)
+{
+    const edit = employee;
+    edit.splice(1,2)[employee];
+
+    return edit;
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee)
+{
+
+}
+
+function updateEmployeeWithKeyAndValue(employee)
+{
+
+}
+
+function deleteFromEmployeeByKey(employee, key, value)
+{
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee)
+{
+
+}
+
+function deleteFromEmployeeByKey(employee)
+{
+
+}
